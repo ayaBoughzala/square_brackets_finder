7bd72ce4fcd9a8eff3032a01240e7f5abce794bb
ayaBoughzala <35595040+ayaBoughzala@users.noreply.github.com>
2021-12-10 22:35:54 +0100

    Update finder.php

--- finder.php
+++ finder.php
@@ -4,9 +4,9 @@ $f = fopen($filename, 'r');
 
 if ($f) {
     $contents = fread($f, filesize($filename));
-    $str = "lorem ipsum [lorem ipsum]lorem ipsum[[[[lorem ipsum]]";
-$pattern = "/[*]/i";
-echo preg_match($pattern, $contents); 
+   
+    $pattern = "/[*]/i";
+    echo preg_match($pattern, $contents); 
     fclose($f);
    
 }
